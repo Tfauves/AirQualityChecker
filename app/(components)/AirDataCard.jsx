ff7e0061f5ce3ee05de19986b88e47885aa19014
Travis J <tfauvelle@gmail.com>
2024-05-22 07:56:27 -0400

    style work

--- app/(components)/AirDataCard.jsx
+++ app/(components)/AirDataCard.jsx
@@ -12,33 +12,47 @@ const AirDataCard = ({ apiData }) => {
   }
 
   let cardClass = "";
+  let actionMessage = "";
   switch (apiData.Category.Number) {
     case 1:
       cardClass = "bg-pollutantCardGood";
+      actionMessage =
+        "Air quality is satisfactory, and air pollution poses little or no risk.";
       break;
     case 2:
       cardClass = "bg-pollutantCardModerate";
+      actionMessage =
+        "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.";
       break;
     case 3:
       cardClass = "bg-pollutantCardSensitive";
+      actionMessage =
+        "Members of sensitive groups may experience health effects. The general public is less likely to be affected.";
       break;
     case 4:
       cardClass = "bg-pollutantCardUnhealthy";
+      actionMessage =
+        "Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.";
       break;
     case 5:
       cardClass = "bg-pollutantCardVeryUnhealthy";
+      actionMessage =
+        "Health alert: The risk of health effects is increased for everyone.";
       break;
     case 6:
       cardClass = "bg-pollutantCardHazardous";
+      actionMessage =
+        "Health warning of emergency conditions: everyone is more likely to be affected.";
       break;
     default:
       cardClass = "bg-pollutantCardUnknown";
+      actionMessage = "";
       break;
   }
 
   return (
     <div
-      className={`flex flex-col ${cardClass} hover:bg-card-hover rounded-md shadow-lg p-3 m-2 `}
+      className={`flex flex-col ${cardClass} hover: rounded-md shadow-lg p-3 m-2 `}
     >
       <div className="flex mb-3">
         <h1>Your Reporting Area</h1>
@@ -48,6 +62,7 @@ const AirDataCard = ({ apiData }) => {
         <div>
           <h3>{apiData.Category.Name}</h3>
           AQI:{apiData.AQI}
+          {actionMessage}
         </div>
       </div>
     </div>
--- app/(components)/AirDataForm.jsx
+++ app/(components)/AirDataForm.jsx
@@ -25,7 +25,7 @@ const AirDataForm = () => {
   const fetchData = async (zip) => {
     const currentDate = new Date().toISOString().slice(0, 10);
     const response = await fetch(
-      `https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=${zip}&date=${currentDate}&distance=25&API_KEY=${apiKEY}`
+      `https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=${zip}&date=${currentDate}&API_KEY=${apiKEY}`
     );
     const result = await response.json();
     setData(result[0]);
@@ -33,14 +33,13 @@ const AirDataForm = () => {
   };
 
   return (
-    <div>
-      <div className="flex justify-center">
+    <div className="mt-32">
+      <div className="flex justify-center  ">
         <form
           className="flex flex-col gap-3 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4"
           onSubmit={handleSubmit}
         >
           <input
-            className="bg-card px-3 py-2 border rounded-md text-sm sm:text-base md:text-lg w-full bg-opacity-20 backdrop-blur-md border-white border-opacity-40 shadow-lg"
             id="zip"
             name="zip"
             type="text"
@@ -49,7 +48,11 @@ const AirDataForm = () => {
             required={true}
             value={formData.zip}
           />
-          <button className="btn" type="submit" value="search zip">
+          <button
+            className="btn max-w-screen-sm"
+            type="submit"
+            value="search zip"
+          >
             Search{" "}
           </button>
         </form>
